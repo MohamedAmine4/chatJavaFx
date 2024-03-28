904b209387855b601e24274464c61bca9523b226
Boussoualef Mohamed Amine <124268899+MohamedAmine4@users.noreply.github.com>
2024-03-28 09:58:08 +0100

    chat

--- javaChat/src/App.java
+++ javaChat/src/App.java
@@ -1,5 +1,30 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+      stage.getIcons().add(new Image("icon.png"));
+        // Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        // Scene scene = new Scene(root);
+        // stage.setTitle("Chat Registration");
+        // stage.setScene(scene);
+        // stage.setResizable(true);
+        // stage.show();
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Chat Rooms");
+        stage.setScene(scene);
+        stage.setResizable(true);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
---
+++ javaChat/src/MainSceneController.java
@@ -0,0 +1,136 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.Node;
+
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXPasswordField;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+public class MainSceneController implements Initializable{
+
+    private Stage stage;
+    private Scene scene;
+  ////////////////////////// Registration //////////////////////////
+
+      @FXML
+      private MFXButton Sign_in;
+  
+      @FXML
+      private MFXPasswordField confirm_password;
+  
+      @FXML
+      private Label hava_account;
+  
+      @FXML
+      private MFXPasswordField password;
+  
+      @FXML
+      private AnchorPane usename;
+  
+      @FXML
+      private MFXTextField username;
+  
+      @FXML
+      private Label warnning;
+  
+      @FXML
+      public void signIn(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Rooms.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+      }
+
+      @Override
+      public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+    
+         }
+  
+  ////////////////////////////// Rooms //////////////////////////////
+  
+
+  
+  
+      @FXML
+      private Button join1;
+  
+      @FXML
+      private Button join2;
+  
+      @FXML
+      private Button join3;
+  
+      @FXML
+      private Button join4;
+  
+      @FXML
+      private Button join5;
+  
+      @FXML
+      private Button join6;
+  
+      @FXML
+      private Label privateChat;
+  
+      @FXML
+      private MFXTextField searchServerName;
+  
+      @FXML
+      void join1(ActionEvent event) {
+  
+      }
+  
+      @FXML
+      void join2(ActionEvent event) {
+  
+      }
+  
+      @FXML
+      void join3(ActionEvent event) {
+  
+      }
+  
+      @FXML
+      void join4(ActionEvent event) {
+  
+      }
+  
+      @FXML
+      void join5(ActionEvent event) {
+  
+      }
+  
+      @FXML
+      void join6(ActionEvent event) {
+  
+      }
+  
+      @FXML
+      void privateChat(MouseEvent event) {
+  
+      }
+  
+      @FXML
+      void searchServerName(ActionEvent event) {
+  
+      }
+  
+  
+  
+}
